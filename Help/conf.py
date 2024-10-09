193fee4e46b5e65db67e21adeacb5bc8cde08172
Sai Anudeepkumar Reddy Danda <anudeepkumar@bvmtechnology.com>
2024-10-09 15:18:33 -0500

    Create conf.py

---
+++ Help/conf.py
@@ -0,0 +1,12 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+project = 'EPMO'
+author = 'BVMDEV'
+
+# -- General configuration ---------------------------------------------------
+extensions = ['recommonmark']  # To support Markdown files
+source_suffix = ['.rst', '.md']  # Accept both .rst and .md files
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'alabaster'  # You can choose a theme of your choice
