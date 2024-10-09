923d194766c84b593936799c8c437b671ede6815
Sai Anudeepkumar Reddy Danda <anudeepkumar@bvmtechnology.com>
2024-10-09 15:40:08 -0500

    Update conf.py

--- Help/conf.py
+++ Help/conf.py
@@ -5,8 +5,8 @@ project = 'EPMO'
 author = 'BVMDEV'
 
 # -- General configuration ---------------------------------------------------
-extensions = ['recommonmark']  # To support Markdown files
-source_suffix = ['.rst', '.md']  # Accept both .rst and .md files
+extensions = ['recommonmark']
+source_suffix = ['.rst', '.md']  # Allow both reStructuredText and Markdown
 
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'alabaster'  # You can choose a theme of your choice
